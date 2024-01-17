845cb96e619b8ef0e3542487257b272cb47f619f
Serdal58 <sserdalyildirim58@gmail.com>
2024-01-17 21:40:38 +0300

    .
    Mvc5 ile 3 Panelli Kütüphane Yönetim Sistemi

---
+++ MvcKutuphne/Controllers/KategoriController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphne.Models.Entity;
+namespace MvcKutuphne.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DBkutuphaneEntities db = new DBkutuphaneEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult Kategoriekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kategoriekle(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+       
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori=db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.TBLKATEGORI.Find(id);
+            return View("KategoriGetir", ktg);
+
+            
+        }
+    }
+}
